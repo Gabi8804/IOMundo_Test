ddbc215dda494263e5b6ef96f646242ba8eaad48
gabri <gabri@DESKTOP-T42UVA3>
2022-11-08 10:37:12 +0200

    Add project files.

---
+++ WcfService/IOfferService.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IOfferService
+    {
+        [OperationContract]
+        List<Offer> SearchAvailability(RequestObject requestObject);
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+  
+    public class Offer
+    {
+        public Offer(DateTime checkInDate, int stayDurationNights, string personCombination,
+            string service_Code, decimal price, decimal pricePerAdult, decimal pricePerChild,
+            decimal strikePrice, decimal strikePricePerAdult, decimal strikePricePerChild, bool showStrikePrice)
+        {
+            CheckInDate = checkInDate;
+            StayDurationNights = stayDurationNights;
+            PersonCombination = personCombination;
+            Service_Code = service_Code;
+            Price = price;
+            PricePerAdult = pricePerAdult;
+            PricePerChild = pricePerChild;
+            StrikePrice = strikePrice;
+            StrikePricePerAdult = strikePricePerAdult;
+            StrikePricePerChild = strikePricePerChild;
+            ShowStrikePrice = showStrikePrice;
+        }
+    
+        public int ID { get; set; }
+
+        public DateTime CheckInDate { get; set; }
+   
+        public int StayDurationNights { get; set; }
+  
+        public string PersonCombination { get; set; }
+        
+        public string Service_Code { get; set; }
+        
+        public decimal Price { get; set; }
+        
+        public decimal PricePerAdult { get; set; }
+        
+        public decimal PricePerChild { get; set; }
+        
+        public decimal StrikePrice { get; set; }
+        
+        public decimal StrikePricePerAdult { get; set; }
+        
+        public decimal StrikePricePerChild { get; set; }
+        
+        public bool ShowStrikePrice { get; set; }
+
+       
+    }
+    [DataContract]
+
+    public class RequestObject
+    {
+        [DataMember]
+        public DateTime DateForm { get; set; }
+        [DataMember]
+        public int Duration { get; set; }
+        [DataMember]
+        public int NumberOfPersons { get; set; }
+        [DataMember]
+        public Credentials Credentials { get; set; }
+    }
+
+    public class Credentials
+    {
+        public Credentials(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ WcfService/OfferService.svc.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Activities;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    
+    public class OfferService : IOfferService
+    {
+        public List<Offer> SearchAvailability(RequestObject requestObject)
+        {
+            Offer offer1 = new Offer(DateTime.Now, 2, "1A", "a", 10, 10, 3, 8, 8, 2, false);
+            Offer offer2 = new Offer(DateTime.Now, 3, "1B", "b", 10, 10, 3, 8, 8, 2, false);
+            Offer offer3 = new Offer(DateTime.Now, 2, "1C", "c", 10, 10, 3, 8, 8, 2, false);
+            Offer offer4 = new Offer(DateTime.Now, 2, "1D", "d", 10, 10, 3, 8, 8, 2, false);
+
+            List<Offer> offers = new List<Offer>();
+
+            offers.Add(offer1);
+            offers.Add(offer2);
+            offers.Add(offer3);
+            offers.Add(offer4);
+
+            Credentials credentials = new Credentials("TestUN", "TestPW");
+
+            List<Offer> offerList = new List<Offer>();
+
+            foreach (Offer offer in offers)
+            {
+                if (requestObject.Duration == offer.StayDurationNights && requestObject.Credentials == credentials)
+                {
+                    offerList.Add(offer);
+                }
+            }
+
+            return offerList;
+        }
+    }
+}
